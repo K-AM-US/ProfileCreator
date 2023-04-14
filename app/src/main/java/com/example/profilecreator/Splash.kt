7d5f9768069e08e155ef483261af2c042a23f869
mauriciocasillas <kamusgallagher@gmail.com>
2023-04-14 15:29:26 -0600

    Splash Screen added. App icon added.

--- app/src/main/java/com/example/profilecreator/Splash.kt
+++ app/src/main/java/com/example/profilecreator/Splash.kt
@@ -1,11 +1,28 @@
 package com.example.profilecreator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.profilecreator.databinding.ActivitySplashBinding
+import kotlin.concurrent.thread
 
 class Splash : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        thread {
+            Thread.sleep(3000)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
+
+
     }
 }
